e9fb912eb9ccd8559bd4be325ad7a489faa129e3
Josh Akeman <joshakeman@gmail.com>
2021-08-08 15:36:46 -0400

    added links to flight menu

--- website/src/components/flightMenu.js
+++ website/src/components/flightMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import flight from '../images/flight-2.png'
+import { Link } from "gatsby"
 
 export default function flightMenu({ open }) {
 
@@ -13,14 +14,24 @@ export default function flightMenu({ open }) {
             ) : (
                 <div className="flight-menu flighted">
                     <img src={flight} />
-                    <div style={{backgroundColor:'red', width:"100%", position:'absolute', bottom: 40, height: 60, opacity: 0.6}} />
-                    <div style={{backgroundColor:'yellow', width:"100%", position:'absolute', bottom: 100, height: 60, opacity: 0.6}} />
-                    <div style={{backgroundColor:'green', width:"100%", position:'absolute', bottom: 160, height: 60, opacity: 0.6}} />
-                    <div style={{backgroundColor:'blue', width:"100%", position:'absolute', bottom: 220, height: 60, opacity: 0.6}} />
-                    <div style={{backgroundColor:'orange', width:"100%", position:'absolute', bottom: 280, height: 60, opacity: 0.6}} />
-                    <div style={{backgroundColor:'turqouise', width:"100%", position:'absolute', bottom: 340, height: 60, opacity: 0.6}} />
-                    <div style={{backgroundColor:'purple', width:"100%", position:'absolute', bottom: 340, height: 60, opacity: 0.6}} />
-
+                    <Link to="/">
+                        <div id="tix-flight-btn" style={{backgroundColor:'red', width:"100%", position:'absolute', bottom: 40, height: 60, opacity: 0.6}} />
+                    </Link>
+                    <Link to="/pix">
+                        <div id="pix-flight-btn" style={{backgroundColor:'yellow', width:"100%", position:'absolute', bottom: 100, height: 60, opacity: 0.6}} />
+                    </Link>
+                    <Link to="/tunes">
+                        <div id="tunes-flight-btn" style={{backgroundColor:'green', width:"100%", position:'absolute', bottom: 160, height: 60, opacity: 0.6}} />
+                    </Link>
+                    <Link to="/brews">
+                        <div id="brews-flight-btn" style={{backgroundColor:'blue', width:"100%", position:'absolute', bottom: 220, height: 60, opacity: 0.6}} />
+                    </Link>
+                    <Link to="/swag">
+                        <div id="swag-flight-btn" style={{backgroundColor:'orange', width:"100%", position:'absolute', bottom: 280, height: 60, opacity: 0.6}} />
+                    </Link>
+                    <Link to="/facts">
+                        <div to="/facts" id="facts-flight-btn" style={{backgroundColor:'purple', width:"100%", position:'absolute', bottom: 340, height: 60, opacity: 0.6}} />
+                    </Link>
                 </div>
             )
         }
