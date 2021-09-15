aa47155fbd6bde2d5769e47df4e474e94798d2c4
Josh Akeman <joshakeman@gmail.com>
2021-09-14 21:46:12 -0400

    changed config

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Suds on the Square`,
+    description: `Suds on the Square beer festival and family event presented by Kiwanis Club of Fayette County.`,
+    author: `Kiwanis Club of Fayette County`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/kiwanis-logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
